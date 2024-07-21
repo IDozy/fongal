3554437cac7214ba39a690c9dcff296999bbde45
Dozy <ahuatayc18_2@unc.edu.pe>
2024-07-21 12:44:54 -0500

    fd

--- app/api/ganado/top3/route.ts
+++ app/api/ganado/top3/route.ts
@@ -1,17 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
-
 export async function GET(request: NextRequest) {
   try {
     const ganado = await prisma.ganado.findMany();
+    console.log("Ganado Data:", ganado); // Verifica los datos obtenidos
 
-    // Convertir el puntaje a número y ordenar
     const top3Ganado = ganado
       .map((g) => ({ ...g, puntaje: g.puntaje ? parseFloat(g.puntaje) : 0 }))
       .sort((a, b) => b.puntaje - a.puntaje)
       .slice(0, 3)
       .map((g, index) => ({ ...g, puesto: index + 1 })); // Añadir el campo puesto
 
+    console.log("Top 3 Ganado:", top3Ganado); // Verifica los datos procesados
     return NextResponse.json(top3Ganado, { status: 200 });
   } catch (error: any) {
     console.error("Error fetching top 3 ganado:", error);
--- app/ganadores/page.tsx
+++ app/ganadores/page.tsx
@@ -1,41 +1,41 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import ListPublic from "./component/ListPublic";
+    "use client";
+    import React, { useEffect, useState } from "react";
+    import ListPublic from "./component/ListPublic";
 
 
-const Student = () => {
-  const [ganadoList, setStudentList] = useState([]);
+    const Student = () => {
+      const [ganadoList, setStudentList] = useState([]);
 
-  useEffect(() => {
-    GetAllStudents();
-  }, []);
+      useEffect(() => {
+        GetAllStudents();
+      }, []);
 
-  const GetAllStudents = () => {
-    const fetchStudents = async () => {
-      try {
-        const response = await fetch("/api/ganado/top3", {
-          method: "GET",
-        });
-        if (!response.ok) {
-          throw new Error("Error fetching ganado");
-        }
+      const GetAllStudents = () => {
+        const fetchStudents = async () => {
+          try {
+            const response = await fetch("/api/ganado/top3", {
+              method: "GET",
+            });
+            if (!response.ok) {
+              throw new Error("Error fetching ganado");
+            }
 
-        const data = await response.json();
-        setStudentList(data)
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
+            const data = await response.json();
+            setStudentList(data)
+          } catch (error) {
+            console.error("Error fetching data: ", error);
+          }
+        };
+        fetchStudents();
+      };
+      return (
+        <div className="p-20 mt-12 pb-20 bg-[#f3fdff]">
+          <h2 className="font-bold text-2xl flex justify-between items-center">
+            Ganadores
+          </h2>
+          <ListPublic ganadoList={ganadoList} refreshData={GetAllStudents} />
+        </div>
+      );
     };
-    fetchStudents();
-  };
-  return (
-    <div className="p-20 mt-12 pb-20 bg-[#f3fdff]">
-      <h2 className="font-bold text-2xl flex justify-between items-center">
-        Ganadores
-      </h2>
-      <ListPublic ganadoList={ganadoList} refreshData={GetAllStudents} />
-    </div>
-  );
-};
 
-export default Student;
+    export default Student;
